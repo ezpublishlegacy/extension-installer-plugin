ae4e9c3b01fd0efcaaeb7b5b5c54233c399aeb66
Laurent RICHARD <easylo@gmail.com>
2014-05-13 15:44:15 +0200

    allow install anything

--- src/easylo/Composer/EzpublishInstaller.php
+++ src/easylo/Composer/EzpublishInstaller.php
@@ -12,18 +12,33 @@ class EzpublishInstaller extends LibraryInstaller
      */
     public function getPackageBasePath(PackageInterface $package)
     {
-        error_log('$package->getExtra()::'.var_export($package->getExtra(),true) ,3, '/home/users/webrichard/tmp/log/PHP_errors.log' ) ;
-        return '../ezpublish_legacy/extensions/'. $package->getPrettyName() ;
-        /*$prefix = substr($package->getPrettyName(), 0, 23);
-        if ('phpdocumentor/template-' !== $prefix) {
-            throw new \InvalidArgumentException(
-                'Unable to install template, phpdocumentor templates '
-                .'should always start their package name with '
-                .'"phpdocumentor/template-"'
-            );
+        
+        $type = $package->getType();
+
+        $prettyName = $package->getPrettyName() ;
+
+        if (strpos($prettyName, '/') !== false) {
+            list($vendor, $name) = explode('/', $prettyName);
+        } else {
+            $vendor = '';
+            $name = $prettyName;
         }
 
-        return 'data/templates/'.substr($package->getPrettyName(), 23);*/
+        $installerPaths = $this->composer->getConfig()->get('installer-paths') ;
+
+        $availableVars = $this->inflectPackageVars(compact('name', 'vendor', 'type'));
+
+        if (!empty( $installerPaths )) {
+                $customPath = $this->mapCustomInstallPaths($installerPaths, $prettyName, $type);
+                if ($customPath !== false) {
+                    return $this->templatePath($customPath, $availableVars);
+                }
+            }
+
+        $this->vendorDir = rtrim($this->composer->getConfig()->get('vendor-dir'), '/');
+        
+        return $this->vendorDir .'/'. $prettyName ;
+        
     }
 
     /**
@@ -32,6 +47,58 @@ class EzpublishInstaller extends LibraryInstaller
     public function supports($packageType)
     {
         return 'library' === $packageType;
-        return 'ezpublish-extension' === $packageType;
+        //return 'ezpublish-extension' === $packageType;
+    }
+
+    /**
+     * For an installer to override to modify the vars per installer.
+     *
+     * @param  array $vars
+     * @return array
+     */
+    public function inflectPackageVars($vars)
+    {
+        return $vars;
+    }
+    
+     /**
+     * Replace vars in a path
+     *
+     * @param  string $path
+     * @param  array  $vars
+     * @return string
+     */
+    protected function templatePath($path, array $vars = array())
+    {
+        if (strpos($path, '{') !== false) {
+            extract($vars);
+            preg_match_all('@\{\$([A-Za-z0-9_]*)\}@i', $path, $matches);
+            if (!empty($matches[1])) {
+                foreach ($matches[1] as $var) {
+                    $path = str_replace('{$' . $var . '}', $$var, $path);
+                }
+            }
+        }
+
+        return $path;
+    }
+
+     /**
+     * Search through a passed paths array for a custom install path.
+     *
+     * @param  array  $paths
+     * @param  string $name
+     * @param  string $type
+     * @return string
+     */
+    protected function mapCustomInstallPaths(array $paths, $name, $type)
+    {
+        foreach ($paths as $path => $names) {
+            if (in_array($name, $names) || in_array('type:' . $type, $names)) {
+                return $path;
+            }
+        }
+
+        return false;
     }
 }
